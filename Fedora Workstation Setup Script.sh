4fa6b5bf13d2c819a746132f84af8a4a067f02a1
NP Viroonc <nathanviroonchatapan@outlook.com>
2022-09-05 02:44:54 -0400

    Update Fedora Workstation Setup Script.sh

--- Fedora Workstation Setup Script.sh
+++ Fedora Workstation Setup Script.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
+clear
 # Start of Function Cluster
+tput setaf 3
+echo "Initializing functions..."
+sleep 1.5
+tput setaf 10
 checkcompatibility () {
 	# Set variables
 	. /etc/os-release
@@ -21,8 +26,8 @@ checkcompatibility () {
 	fi
 	isworkstation="true"
 
-	# Check for 35
-	if ! echo $VERSION_ID | grep -qi "35"
+	# Check for 36
+	if ! echo $VERSION_ID | grep -qi "36"
 	then
 		sysreqfail
 	fi
@@ -33,10 +38,11 @@ checkcompatibility () {
 		sysreqfail
 	fi
 }
+echo "Loaded checkcompatibility."
 sysreqfail () {
 	clear
 	tput setaf 9
-	echo "System requirements not met. This script supports the x86_64 version of Fedora 35 Workstation!!!"
+	echo "System requirements not met. This script supports the x86_64 version of Fedora 36 Workstation!!!"
 	tput setaf 3
 	echo "If your error is not caused by a wrong Fedora version or OS architecture, please check to see if I have published a script for your system."
 	tput setaf 10
@@ -54,13 +60,14 @@ sysreqfail () {
 	read -sN1 answer
 	quitscript
 }
+echo "Loaded sysreqfail."
 mainmenu () {
 	clear
  	tput setaf 3
-	echo "=============================================="
-	echo " --- Fedora Workstation Setup Script 4.15 ---"
-	echo "=============================================="
-	echo "Supported Fedora Workstation Versions (x86_64): 35"
+	echo "============================================="
+	echo " --- Fedora Workstation Setup Script 5.0 ---"
+	echo "============================================="
+	echo "Supported Fedora Workstation Versions (x86_64): 36"
 	tput setaf 10
 	echo "Your current distro is $PRETTY_NAME."
 	echo "Your current Fedora version is $VERSION_ID."
@@ -76,7 +83,7 @@ tput setaf 3
 	echo "System will automatically reboot after the script is run!!!"
 	echo "It is not recommended to run this script more than once!!!"
 	tput setaf 10
-	echo "You may run this script again after an upgrade or to get your system up-to-date with the latest version of my script."
+	echo "You may run this script again after a failure, an upgrade, or to get your system up-to-date with the latest version of my script."
 	tput setaf 9
 	echo "Make sure you have a stable and fast Internet connection before proceeding!!!"
 	tput setaf 3
@@ -97,6 +104,7 @@ tput setaf 3
 		*)	badoption;;
 	esac
 }
+echo "Loaded mainmenu."
 multiusermenu () {
 	clear
  	tput setaf 3
@@ -111,11 +119,13 @@ multiusermenu () {
 	read -sN1 answer
 	mainmenu
 }
+echo "Loaded multiusermenu."
 quitscript () {
 	tput sgr0
 	clear
 	exit
 }
+echo "Loaded quitscript."
 badoption () {
 	clear
 	tput setaf 9
@@ -126,6 +136,7 @@ badoption () {
 	sleep 3
 	mainmenu
 }
+echo "Loaded badoption."
 finish () {
 	clear
 	tput setaf 10
@@ -137,6 +148,7 @@ finish () {
 	clear
 	sudo reboot
 }
+echo "Loaded finish."
 full () {
 	clear
 	tput setaf 3
@@ -144,41 +156,48 @@ full () {
 	tput sgr0
 	sleep 3
 	clear
-	sudo dnf install -y https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
-	sudo dnf groupupdate -y core
-	sudo dnf groupupdate -y multimedia --setop="install_weak_deps=False" --exclude=PackageKit-gstreamer-plugin
-	sudo dnf groupupdate -y sound-and-video
-	sudo dnf install -y rpmfusion-free-release-tainted
-	sudo dnf install -y libdvdcss
-	sudo dnf install -y rpmfusion-nonfree-release-tainted
-	sudo dnf mark -y install libfreeaptx pipewire-codec-aptx
-	flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
-	sudo dnf install -y curl cabextract xorg-x11-font-utils fontconfig
-	sudo rpm -i https://downloads.sourceforge.net/project/mscorefonts2/rpms/msttcore-fonts-installer-2.6-1.noarch.rpm
-	sudo dnf install -y alien remmina bleachbit frozen-bubble asunder brasero k3b libburn cdrskin pavucontrol easyeffects rhythmbox rhythmbox-alternative-toolbar shotwell solaar gnome-boxes gparted vlc p7zip* gnome-tweaks gnome-extensions-app chrome-gnome-shell lame gpart neofetch ffmpeg httrack tree telegram-desktop easytag android-tools gnome-sound-recorder cheese supertux dconf-editor deja-dup gnome-todo sushi unoconv ffmpegthumbs gnome-books krita gnome-clocks gimp htop transmission curl git handbrake-gui minetest obs-studio discord menulibre libreoffice-draw java-latest-openjdk gstreamer-plugins* gstreamer1-plugins* pip shotcut google-chrome-stable kernel-headers kernel-devel gcc glibc-headers make dkms file-roller file-roller-nautilus cpu-x gucharmap gnome-power-manager bijiben epiphany
+	runcheck sudo dnf install -y https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+	runcheck sudo dnf groupupdate -y core
+	runcheck sudo dnf groupupdate -y multimedia --setop="install_weak_deps=False" --exclude=PackageKit-gstreamer-plugin
+	runcheck sudo dnf groupupdate -y sound-and-video
+	runcheck sudo dnf install -y rpmfusion-free-release-tainted
+	runcheck sudo dnf install -y libdvdcss
+	runcheck sudo dnf install -y rpmfusion-nonfree-release-tainted
+	runcheck sudo dnf mark -y install libfreeaptx pipewire-codec-aptx
+	runcheck flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+	runcheck sudo dnf install -y curl cabextract xorg-x11-font-utils fontconfig
+	runcheck sudo dnf install -y "https://downloads.sourceforge.net/project/mscorefonts2/rpms/msttcore-fonts-installer-2.6-1.noarch.rpm"
+	runcheck sudo dnf install -y alien remmina bleachbit frozen-bubble asunder brasero k3b libburn cdrskin pavucontrol easyeffects rhythmbox rhythmbox-alternative-toolbar shotwell solaar gnome-boxes gparted vlc p7zip* gnome-tweaks gnome-extensions-app chrome-gnome-shell lame gpart neofetch ffmpeg httrack tree telegram-desktop easytag android-tools gnome-sound-recorder cheese supertux dconf-editor deja-dup gnome-todo sushi unoconv ffmpegthumbs gnome-books krita gnome-clocks gimp htop fragments curl git handbrake-gui minetest obs-studio discord menulibre libreoffice-draw java-latest-openjdk gstreamer-plugins* gstreamer1-plugins* pip shotcut google-chrome-stable kernel-headers kernel-devel gcc glibc-headers make dkms file-roller file-roller-nautilus cpu-x gucharmap gnome-power-manager bijiben libheif libquicktime gdk-pixbuf2 mcomix3 VirtualBox gscan2pdf supertuxkart unzip
 	javamenu
-	sudo dnf update -y --refresh
-	sudo dnf autoremove -y
-	flatpak install -y flathub com.system76.Popsicle
-	flatpak install -y flathub org.audacityteam.Audacity
-	flatpak install -y flathub org.musescore.MuseScore
-	flatpak install -y flathub com.mojang.Minecraft
-	flatpak install -y flathub org.inkscape.Inkscape
-	flatpak install -y flathub ar.xjuan.Cambalache
-	flatpak install -y flathub com.github.jeromerobert.pdfarranger
-	flatpak install -y flathub com.github.muriloventuroso.pdftricks
-	flatpak install -y flathub org.kde.okular
-	flatpak install -y flathub com.github.flxzt.rnote
-	flatpak install -y flathub com.github.tchx84.Flatseal
-	flatpak install -y flathub com.mattjakeman.ExtensionManager
-	flatpak install -y flathub org.gnome.TextEditor
-	flatpak update -y
-	flatpak uninstall -y --unused --delete-data
-	pip install pip youtube-dl yt-dlp speedtest-cli -U
+	runcheck sudo dnf update -y --refresh
+	runcheck sudo dnf autoremove -y
+	runcheck flatpak install -y flathub com.system76.Popsicle
+	runcheck flatpak install -y flathub org.audacityteam.Audacity
+	runcheck flatpak install -y flathub org.musescore.MuseScore
+	runcheck flatpak install -y flathub com.mojang.Minecraft
+	runcheck flatpak install -y flathub org.inkscape.Inkscape
+	runcheck flatpak install -y flathub ar.xjuan.Cambalache
+	runcheck flatpak install -y flathub com.github.jeromerobert.pdfarranger
+	runcheck flatpak install -y flathub com.github.muriloventuroso.pdftricks
+	runcheck flatpak install -y flathub org.kde.okular
+	runcheck flatpak install -y flathub com.github.flxzt.rnote
+	runcheck flatpak install -y flathub com.github.tchx84.Flatseal
+	runcheck flatpak install -y flathub com.mattjakeman.ExtensionManager
+	runcheck flatpak install -y flathub com.wps.Office
+	runcheck flatpak install -y flathub app.drey.EarTag
+	runcheck flatpak update -y
+	runcheck flatpak uninstall -y --unused --delete-data
+	runcheck pip install pip wheel youtube-dl yt-dlp speedtest-cli mangadex-downloader pillow py7zr animdl -U
+    runcheck pip cache purge
 	echo "Adding current user to cdrom group..."
-	sudo usermod -aG cdrom $USER
+	runcheck sudo usermod -aG cdrom $USER
+	echo "Adding current user to vboxusers group..."
+	runcheck sudo usermod -aG vboxusers $USER
+	autofontinstall
+	installadwtheme
 	finish
 }
+echo "Loaded full."
 minimal () {
 	clear
 	tput setaf 3
@@ -186,31 +205,35 @@ minimal () {
 	tput sgr0
 	sleep 3
 	clear
-	sudo dnf install -y https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
-	sudo dnf groupupdate -y core
-	sudo dnf groupupdate -y multimedia --setop="install_weak_deps=False" --exclude=PackageKit-gstreamer-plugin
-	sudo dnf groupupdate -y sound-and-video
-	sudo dnf install -y rpmfusion-free-release-tainted
-	sudo dnf install -y libdvdcss
-	sudo dnf install -y rpmfusion-nonfree-release-tainted
-	sudo dnf mark -y install libfreeaptx pipewire-codec-aptx
-	flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
-	sudo dnf install -y curl cabextract xorg-x11-font-utils fontconfig
-	sudo rpm -i https://downloads.sourceforge.net/project/mscorefonts2/rpms/msttcore-fonts-installer-2.6-1.noarch.rpm
-	sudo dnf install -y alien pavucontrol rhythmbox rhythmbox-alternative-toolbar gparted p7zip* gnome-tweaks gnome-extensions-app gpart ffmpeg dconf-editor deja-dup sushi unoconv ffmpegthumbs htop curl git menulibre gstreamer-plugins* gstreamer1-plugins* pip google-chrome-stable kernel-headers kernel-devel gcc glibc-headers make dkms file-roller file-roller-nautilus easyeffects cpu-x gucharmap gnome-power-manager bijiben epiphany
-	sudo dnf update -y --refresh
-	sudo dnf autoremove -y
-	flatpak install -y flathub com.github.jeromerobert.pdfarranger
-	flatpak install -y flathub com.github.muriloventuroso.pdftricks
-	flatpak install -y flathub org.kde.okular
-	flatpak install -y flathub com.github.tchx84.Flatseal
-	flatpak install -y flathub com.mattjakeman.ExtensionManager
-	flatpak install -y flathub org.gnome.TextEditor
-	flatpak update -y
-	flatpak uninstall -y --unused --delete-data
-	pip install pip speedtest-cli -U
+	runcheck sudo dnf install -y https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+	runcheck sudo dnf groupupdate -y core
+	runcheck sudo dnf groupupdate -y multimedia --setop="install_weak_deps=False" --exclude=PackageKit-gstreamer-plugin
+	runcheck sudo dnf groupupdate -y sound-and-video
+	runcheck sudo dnf install -y rpmfusion-free-release-tainted
+	runcheck sudo dnf install -y libdvdcss
+	runcheck sudo dnf install -y rpmfusion-nonfree-release-tainted
+	runcheck sudo dnf mark -y install libfreeaptx pipewire-codec-aptx
+	runcheck flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+	runcheck sudo dnf install -y curl cabextract xorg-x11-font-utils fontconfig
+	runcheck sudo dnf install -y "https://downloads.sourceforge.net/project/mscorefonts2/rpms/msttcore-fonts-installer-2.6-1.noarch.rpm"
+	runcheck sudo dnf install -y alien pavucontrol rhythmbox rhythmbox-alternative-toolbar gparted p7zip* gnome-tweaks gnome-extensions-app gpart ffmpeg dconf-editor deja-dup sushi unoconv ffmpegthumbs htop curl git menulibre gstreamer-plugins* gstreamer1-plugins* pip google-chrome-stable kernel-headers kernel-devel gcc glibc-headers make dkms file-roller file-roller-nautilus easyeffects cpu-x gucharmap gnome-power-manager bijiben libheif libquicktime gdk-pixbuf2 mcomix3 gscan2pdf unzip
+	runcheck sudo dnf update -y --refresh
+	runcheck sudo dnf autoremove -y
+	runcheck flatpak install -y flathub com.github.jeromerobert.pdfarranger
+	runcheck flatpak install -y flathub com.github.muriloventuroso.pdftricks
+	runcheck flatpak install -y flathub org.kde.okular
+	runcheck flatpak install -y flathub com.github.tchx84.Flatseal
+	runcheck flatpak install -y flathub com.mattjakeman.ExtensionManager
+	runcheck flatpak install -y flathub com.wps.Office
+	runcheck flatpak update -y
+	runcheck flatpak uninstall -y --unused --delete-data
+	runcheck pip install pip wheel speedtest-cli -U
+    runcheck pip cache purge
+    autofontinstall
+    installadwtheme
 	finish
 }
+echo "Loaded minimal."
 javamenu () {
 	clear
  	tput setaf 3
@@ -223,12 +246,71 @@ javamenu () {
 	IFS=""
 	read -sN1 answer
 	clear
-	sudo alternatives --config java
+	runcheck sudo alternatives --config java
 	clear
 	java --version
 	sleep 3
 	clear
 }
+echo "Loaded javamenu."
+autofontinstall () {
+	echo "Installing the Essential Font Pack..."
+	runcheck sudo wget -O "/tmp/fontinstall.zip" "https://github.com/TechnologyMan101/script-extras/releases/download/20220822-0943/Essential.Font.Pack.zip"
+	runcheck sudo unzip -o "/tmp/fontinstall.zip" -d "/usr/share/fonts"
+	runcheck sudo chmod -R 755 "/usr/share/fonts/Essential Font Pack"
+	runcheck sudo rm "/tmp/fontinstall.zip"
+}
+echo "Loaded autofontinstall."
+installadwtheme () {
+	echo "Installing the Adwaita theme set..."
+	runcheck flatpak install -y flathub com.github.GradienceTeam.Gradience
+	runcheck sudo dnf copr enable -y nickavem/adw-gtk3
+	runcheck sudo dnf install -y adw-gtk3
+	runcheck flatpak install -y org.gtk.Gtk3theme.adw-gtk3 org.gtk.Gtk3theme.adw-gtk3-dark
+	# Set default light theme
+	runcheck gsettings set org.gnome.desktop.interface gtk-theme 'adw-gtk3'
+	runcheck gsettings set org.gnome.desktop.interface color-scheme 'default'
+}
+echo "Loaded installadwtheme."
+runcheck () {
+	IFS=$'\n'
+	command="$*"
+	retval=1
+	attempt=1
+	until [[ $retval -eq 0 ]] || [[ $attempt -gt 5 ]]; do
+		(
+			set +e
+			$command
+		)
+		retval=$?
+		attempt=$(( $attempt + 1 ))
+		if [[ $retval -ne 0 ]]; then
+			clear
+			tput setaf 9
+			echo "Oops! Something went wrong! Retrying in 3 seconds..."
+			tput sgr0
+			sleep 3
+			clear
+		fi
+	done
+	if [[ $retval -ne 0 ]] && [[ $attempt -gt 5 ]]; then
+		clear
+		tput setaf 9
+		echo "Oops! A fatal error has occurred and the program cannot continue. Returning to the main menu in 10 seconds..."
+		tput setaf 3
+		echo "Please try again later or if the problem persists, create an issue on GitHub."
+		tput sgr0
+		sleep 10
+		clear
+		mainmenu
+	fi
+	IFS=""
+}
+echo "Loaded runcheck."
+tput setaf 3
+echo "Continuing..."
+tput sgr0
+sleep 1.5
 # End of Function Cluster
 # Start of Main Script
 while true
